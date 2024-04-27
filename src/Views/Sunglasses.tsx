bd4c82035cd7e14b12963a6d46f8c818aec18a3e
Andrea Ceccarelli <gltch2003@gmail.com>
2024-04-27 12:24:04 +0200

    skeleton

---
+++ src/Views/Sunglasses.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Layout } from "../components/Layout";
+
+export const Sunglasses: React.FC = () => {
+  return (
+    <Layout>
+      <div>SUNGLASSES</div>
+    </Layout>
+  );
+};
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./index.css";
+import { Link } from "react-router-dom";
 
 export const Header: React.FC = () => {
   return (
     <header className="App-header">
       <div className="App-logo">Logo</div>
       <div className="buttons-group">
-        <button>SUNGLASSES</button>
-        <button>EYEGLASSES</button>
-        <button>LENSES</button>
+        <Link to="/sunglasses">SUNGLASSES</Link>
+        <Link to="/eyeglasses">EYEGLASSES</Link>
+        <Link to="/lenses">LENSES</Link>
       </div>
       <div className="search">
         <input placeholder="Search"></input>
